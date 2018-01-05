a7bcc10ee27b72ef71eb4c01aa537a61e99be7ba
jodom <jodomouma@gmail.com>
2018-01-05 16:05:56 +0300

    first unittest with url mapping and dummy view

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+import series
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', series.views.home, name='home')
 ]
--- series/tests.py
+++ series/tests.py
@@ -1,8 +1,16 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
-
+from .views import home
 # Create your tests here.
 
 class SmokeTest(TestCase):
 
     def test_quick_math(self):
         self.assertEqual(2+2-1, 3)
+
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home)
