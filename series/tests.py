357af2383146f0d03446561fd840700f663ab6c1
jodom <jodomouma@gmail.com>
2018-01-05 16:35:32 +0300

    Basic view with minimal html

--- series/tests.py
+++ series/tests.py
@@ -1,5 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from .views import home
 # Create your tests here.
 
@@ -14,3 +16,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>Seris, a note a day</title>',
+                      response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
--- series/views.py
+++ series/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def home():
-    pass
+def home(request):
+    return HttpResponse('<html><title>Seris, a note a day</title></html>')
