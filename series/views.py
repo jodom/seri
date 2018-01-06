dda024f864b8594b13ef460a67d8da6306b99e0c
jodom <jodomouma@gmail.com>
2018-01-06 18:28:25 +0300

    Refactor home page view to use template

--- series/views.py
+++ series/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<html><title>Seris, a note a day</title></html>')
+    return render(request, 'seris/home.html')
