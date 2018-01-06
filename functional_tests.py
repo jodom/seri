fa0c9ba69bb0ecd55b55896045bb5d40a5f9ffcc
jodom <jodomouma@gmail.com>
2018-01-06 18:01:55 +0300

    FT now checks we can create a serie and save notes

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewSerieTest(unittest.TestCase):
@@ -19,24 +20,38 @@ class NewSerieTest(unittest.TestCase):
 
         # He notices that the page title and header mention Seris - the note taking app
         self.assertIn('Seris, a note a day', self.browser.title)
-        self.fail('Finich the test!')
+
+        header = self.browser.find_element_by_tag_name('h1').text
+
+        self.assertIn('Seris', header)
+
 
         # He is  invited to create a serie straight a way
+        seriebox = self.browser.find_element_by_id('id_new_serie')
+        self.assertEqual(seriebox.get_attribute('placeholder'), 'Create a new Serie')
 
-        # He types " Notes to future self" into a text box
+        # He types "Notes to future self" into a text box
+        seriebox.send_keys('Notes to future self')
 
         # When he hits enter, the page updates with a new listing
         # "Notes to future self" as a new serie
+        seriebox.send_keys(Keys.ENTER)
 
         # There is a text box inviting him to add a note to the serie
+        notebox = self.browser.find_element_by_id('id_new_note')
+        self.assertEqual(notebox.get_attribute('placeholder'), 'New note')
 
         # He enters, " My first note" and presses enter
+        notebox.send_keys('My first note').send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_notes_table')
 
         # Now the serie has a single note : "My first Note, Date"
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == 'My first note') for row in rows)
 
         # There is still a text box inviting him to add another note to the serie
-
         # He enters, " That was easy" and presses enter
+        self.fail('Finich the test!')
 
         # The page updates again, and now both notes are listed
 
