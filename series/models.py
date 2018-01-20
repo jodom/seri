54c8c561391bf31ed15d3eaf0f48299046891dde
jodom <jodomouma@gmail.com>
2018-01-20 08:33:00 +0300

    create a Note model

--- series/models.py
+++ series/models.py
@@ -15,3 +15,15 @@ class Serie(models.Model):
 
     def __str__(self):
         return self.title
+
+class Note(models.Model):
+    serie = models.ForeignKey('Serie')
+    content = models.TextField(blank=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.content
