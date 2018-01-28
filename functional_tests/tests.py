b59cf15ba45f85e3ff82ac5832c8ec485c6b524e
jodom <jodomouma@gmail.com>
2018-01-28 17:53:22 +0300

    A note first approach to functionality

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -11,14 +11,23 @@ class NewSerieTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def create_new_note(self, text):
+        ''' type and save a note '''
+        notebox = self.browser.find_element_by_id('id_new_note')
+        self.assertEqual(notebox.get_attribute('placeholder'), 'New note')
+
+        notebox.send_keys(text)
+        notebox.send_keys(Keys.ENTER)
+
     def lookup_a_note_in_a_serie(self, text):
+        ''' confirm the existence of a note in a serie '''
         notes_list = self.browser.find_element_by_tag_name('ul')
         notes = notes_list.find_elements_by_tag_name('li')
 
         self.assertIn(text, [note.content for note in notes])
         self.assertTrue(any(note.content == text for note in notes))
 
-    def test_can_start_a_serie_and_see_it_later(self):
+    def test_can_save_a_quick_note_and_see_it_later(self):
         # Jodom has been intospecting a lot lately
         # He has bult an app to capture his thoughts in the form of short notes
         # So he goes home and checks the homepage
@@ -35,45 +44,30 @@ class NewSerieTest(LiveServerTestCase):
         sub_header = self.browser.find_element_by_id('id_sub_header').text
         self.assertEqual('a note a day', sub_header)
 
-        # Under the catchphrase, there is a link inviting him to create a serie
-        new_serie_link = self.browser.find_element_by_id('id_new_serie')
-
-        # He clicks on it and is taken to a new page where he can create a serie
-        new_serie_link.send_keys(Keys.ENTER)
-
-        # He is  invited to create a serie straight a way
-        seriebox = self.browser.find_element_by_id('id_title_input')
-        self.assertEqual(seriebox.get_attribute('placeholder'), 'Create a new Serie')
-
-        # He types "Notes to future self" into a text box
-        seriebox.send_keys('Notes to future self')
-
-        # When he hits enter, the page updates with a new listing
-        # "Notes to future self" as a new serie
-        seriebox.send_keys(Keys.ENTER)
-
-        # The new Serie is created, with a page showing its title now being displayed
+        # There is a box inviting Jodom to create a note straight away
+        # He types 'This is a quick note' and presses ENTER
+        self.create_new_note('This is a quick note')
+        
+        # The page updates and he sees a page with the heading 'My Notes' and his note listed under it
         title = self.browser.find_element_by_id('id_serie_title')
-        self.assertEqual(title.text, 'Notes to future self')
+        self.assertEqual(title.text, 'My Notes')
 
-        # There is a text box inviting him to add a note to the serie
-        notebox = self.browser.find_element_by_id('id_new_note')
-        self.assertEqual(notebox.get_attribute('placeholder'), 'New note')
+        self.lookup_a_note_in_a_serie('This is a quick note')
 
+        # There is a text box inviting him to add another note to the serie
         # He enters, " My first note" and presses enter
-        notebox.send_keys('My first note')
-        notebox.send_keys(Keys.ENTER)
+        self.create_new_note('My second note')
 
         # Now the serie has a single note : "My first Note"
-        self.lookup_a_note_in_a_serie('My first note')
+        self.lookup_a_note_in_a_serie('My second note')
 
         # There is still a text box inviting him to add another note to the serie
         # He enters, " That was easy" and presses enter
-        notebox.send_keys('That was easy')
-        notebox.send_keys(Keys.ENTER)
+        self.create_new_note('That was easy')
 
-        # The page updates again, and now both notes are listed
-        self.lookup_a_note_in_a_serie('My first note')
+        # The page updates again, and now both notes are listed under the quick note
+        self.lookup_a_note_in_a_serie('This is a quick note')
+        self.lookup_a_note_in_a_serie('My second note')
         self.lookup_a_note_in_a_serie('That was easy')
 
         # Jodom wonders whether the site will remember his notes
